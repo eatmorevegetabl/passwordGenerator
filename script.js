7c339a644c328c1a36f06f5962e28516ea692f48
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2022-08-02 23:42:33 -0600

    created password length requirements and corresponding prompts

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Password length prompts, between 8-128 characters
+var passwordLength = Number(window.prompt("How many characters should this password have?\nMust be between 8 - 128"));
+
+if (passwordLength >= 8 && passwordLength <= 128) {
+  window.alert(`Your password is going to be ${passwordLength} characters long.`);
+} else {
+  window.alert("Please enter a number between 8 - 128");
+}
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  var passwordChar; //outside function
+
   passwordText.value = password;
 
-  prompt() //Length 8-128
-  prompt() // Lowercase, Uppercase, numeric, special characters
-  prompt() // Input validated
 }
+
+// Validate input
+//prompt() // Lowercase, Uppercase, numeric, special characters
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
