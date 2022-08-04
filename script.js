7f128d23041d9cf728a5a5de5c5c4873e99356e4
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2022-08-03 22:30:25 -0600

    created selectLength function

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Password length prompts, between 8-128 characters
-var passwordLength = Number(window.prompt("How many characters should this password have?\nMust be between 8 - 128"));
+var passwordLength;
 
-if (passwordLength >= 8 && passwordLength <= 128) {
-  window.alert(`Your password is going to be ${passwordLength} characters long.`);
-} else {
-  window.alert("Please enter a number between 8 - 128");
+//Password character type
+var charType = window.prompt();
+
+var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+var uppercase = lowercase.toUpperCase();
+var numeric = Number[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialCharacters = String[" !#$%&'()*+,-./:;<=>?@[]^_`{|}~" + '"' + "\\"];
+
+
+//generatePassword function
+function generatePassword() {
+
 }
-
-
-
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  var passwordChar; //outside function
-
   passwordText.value = password;
-
 }
-
-// Validate input
-//prompt() // Lowercase, Uppercase, numeric, special characters
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function selectLength() {
+  //Password length prompts, between 8-128 characters
+  passwordLength = Number(window.prompt("How many characters should this password have?\nMust be between 8 - 128"));
+
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    window.alert(`Your password is going to be ${passwordLength} characters long.`);
+  } else {
+    window.alert("Please enter a number between 8 - 128");
+  }
+}
