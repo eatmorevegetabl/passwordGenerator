1b4ff5994cbea191dff3880d7f927fc8d8ef61d8
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2022-08-04 22:40:13 -0600

    added if statements + prompts for every character type, created finalCharacterArray to store all characters user wants

--- script.js
+++ script.js
@@ -1,20 +1,60 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var passwordLength;
-var passwordCharacters;
+var finalCharacterArray;
 
 function selectChar() {
-  //Character type prompt, at least 1 char type must be selected:
-  //lowercase, uppercase, numeric, or special characters
-  passwordCharacters = String(window.prompt("What type of characters would you like in your password?\nSelect from: Uppercase, lowercase, numeric, or special characters"));
   var alphabet = 'abcdefghijklmnopqrstuvwxyz';
   var lowercase = alphabet.split(''); //lowercase alphabet array
   var uppercase = alphabet.toUpperCase().split(''); //uppercase alphabet array
   var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; //numeric array
   var list = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
   var specialCharacters = list.split(''); //special character array
+  
+  //Character type prompt, at least 1 char type must be selected:
+  //lowercase, uppercase, numeric, or special characters
+      finalCharacterArray = [];
+  var lowerAnswer = window.confirm("Would you like lowercase characters?");
+  if (lowerAnswer) {
+      console.log("Answer is yes");
+      finalCharacterArray.push(...lowercase); //lowercase appended to finalcharacterarray
+  } else {
+    console.log("Answer is no");
+  }
+  
+  var upperAnswer = window.confirm("Would you like uppercase characters?");
+  if (upperAnswer) {
+      console.log("Answer is yes");
+      finalCharacterArray.push(...uppercase); //uppercase appended to finalcharacterarray
+  } else {
+    console.log("Answer is no");
+  }
 
-}
+  var numericAnswer = window.confirm("Would you like numeric characters?");
+  if (numericAnswer) {
+      console.log("Answer is yes");
+      finalCharacterArray.push(...numeric); //numeric appended to finalcharacterarray
+  } else {
+    console.log("Answer is no");
+  }
+
+  var specialAnswer = window.confirm("Would you like special characters?");
+  if (specialAnswer) {
+      console.log("Answer is yes");
+      finalCharacterArray.push(...specialCharacters); //specialCharacters appended to finalcharacterarray
+  } else {
+    console.log("Answer is no");
+  }
+    console.log(finalCharacterArray.length);
+    
+  //   if (passwordCharacters === "Uppercase" || passwordCharacters === "lowercase" || passwordCharacters === "numeric") {
+  //   window.alert("Your password will contain " + passwordCharacters + " characters.");
+  // } else if (passwordCharacters === "special characters") {
+  //   window.alert("Your password will contain " + passwordCharacters + ".");
+  // } else {
+  //   window.alert("Please enter one of the four type of characters");
+  // }
+};
 
 function selectLength() {
   //Password length prompt, between 8-128 characters
@@ -25,21 +65,27 @@ function selectLength() {
   } else {
     window.alert("Please enter a number between 8 - 128");
   }
-}
+};
 
-//generatePassword function
-//function generatePassword() {
-  
-//}
+// generatePassword function
+function generatePassword() {
+  var randomArray = [];
+  for (i = 0; i < passwordLength; i++) {
+    randomArray[i] = Math.floor(Math.random()*finalCharacterArray);
+  }
+  console.log(passwordLength);
+  console.log(randomArray);
+  return randomArray;
+}
 
 // Write password to the #password input
 function writePassword() {
   selectLength();
   selectChar();
-  
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+
   passwordText.value = password;
 }
 
